994f4f38f5ca74cefc90882ac06bfb51baff2adf
thward7 <7thwardmgmt@gmail.com>
2024-06-21 12:55:52 +0500

    feat: add drop-down list for buttons

---
+++ src/components/DropdownList.tsx
@@ -0,0 +1,12 @@
+const DropdownList = () => {
+  return (
+    <div className='dropdown-content *:py-1'>
+      <button>Week</button>
+      <button>Month</button>
+      <button>Year</button>
+      <button>All time</button>
+    </div>
+  )
+}
+
+export default DropdownList
--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,11 +1,30 @@
+import { useState } from 'react'
+import DropdownList from './DropdownList'
+
 const Filters = () => {
+  const [showDropdown, setShowDropdown] = useState(false)
+
   return (
     <div className='flex items-center justify-between p-2 max-sm:flex-col max-sm:gap-y-2'>
-      <div className='text-lg'>Tutorials</div>
+      <div className='text-lg max-sm:hidden'>Tutorials</div>
       <div className='flex gap-3'>
-        <button className='gray-box hover:underline'>New</button>
-        <button className='gray-box hover:underline'>Top rate</button>
-        <button className='gray-box hover:underline'>Top views</button>
+        <button className='gray-box text-orange hover:underline'>New</button>
+        <div
+          className='dropdown'
+          onMouseOut={() => setShowDropdown(false)}
+          onMouseOver={() => setShowDropdown(true)}
+        >
+          <button className='gray-box'>Top rate</button>
+          {showDropdown && <DropdownList />}
+        </div>
+        <div
+          className='dropdown'
+          onMouseOut={() => setShowDropdown(false)}
+          onMouseOver={() => setShowDropdown(true)}
+        >
+          <button className='gray-box'>Top views</button>
+          {showDropdown && <DropdownList />}
+        </div>
       </div>
     </div>
   )
